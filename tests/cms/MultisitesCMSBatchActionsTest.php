75532828525fc72d32597376150f7a5561d8f719
Jake Bentvelzen <jake@silverstripe.com.au>
2017-03-28 11:00:08 +1100

    test 19

---
+++ tests/cms/MultisitesCMSBatchActionsTest.php
@@ -0,0 +1,8 @@
+<?php
+
+class MultisitesCMSBatchActionsTest extends CMSBatchActionsTest {
+	public function testBatchRestore() {
+		$this->markTestSkipped(__FUNCTION__.' not implemented for Multisites.');
+		return;
+	}
+}
